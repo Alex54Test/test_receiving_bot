e94608ff49bfa914cdb4c1334fe97314e5929aea
Alexander Konopliov <AlexanderKonopliov81@gmail.com>
2025-07-14 12:43:53 +0300

    Completely finished the work select branch in the main workshop

--- app/keyboards/works_selesct_main_WS_kbs.py
+++ app/keyboards/works_selesct_main_WS_kbs.py
@@ -4,7 +4,7 @@ bunHandling = InlineKeyboardMarkup(inline_keyboard=[                            
     [InlineKeyboardButton(text='Видавлювання плюшки', callback_data='extrusion_holes'),
      InlineKeyboardButton(text='Свердлівка отворів', callback_data='drilling_holes')],
     [InlineKeyboardButton(text='Зачистка отворів', callback_data='cleaning_holes'),
-     InlineKeyboardButton(text='Заклепування плюшок', callback_data='riveting buns')],
+     InlineKeyboardButton(text='Заклепування плюшок', callback_data='riveting_buns')],
     [InlineKeyboardButton(text='Герметиз. ложемента', callback_data='cradle_sealing'),
      InlineKeyboardButton(text='Встановлення пушечки', callback_data='gun_installation')],
     [InlineKeyboardButton(text='Установка стопорних\n кілець на пушки', callback_data='installation_retaining_rings'),
@@ -17,19 +17,21 @@ bunHandling = InlineKeyboardMarkup(inline_keyboard=[                            
      InlineKeyboardButton(text='Розкладання плюшек', callback_data='transfer_buns')],
     [InlineKeyboardButton(text='Пакування плюшек', callback_data='packaging_buns')],
 ])
+
 makingBagels = InlineKeyboardMarkup(inline_keyboard=[                                # вибір робіт
     [InlineKeyboardButton(text='Нарізка гуми', callback_data='cutting_rubber'),
      InlineKeyboardButton(text='Проклеювання бублика', callback_data='gluing_bagel')],
-    [InlineKeyboardButton(text='Свердлівка бублика', callback_data='Bagel drill'),
+    [InlineKeyboardButton(text='Свердлівка бублика', callback_data='bagel_drill'),
      InlineKeyboardButton(text='Встановлення каналів\n та штирів', callback_data='channels&pins')],
     [InlineKeyboardButton(text='Проклеювання каналів', callback_data='gluing_channels'),
      InlineKeyboardButton(text='Герметизація каналів', callback_data='sealing_channels')],
     [InlineKeyboardButton(text='Зачистка бублика', callback_data='cleaning_bagel'),
      InlineKeyboardButton(text='Вибірковий тех.\n контроль бублика', callback_data='selective_control')]
 ])
+
 stamping = InlineKeyboardMarkup(inline_keyboard=[                                # вибір робіт
     [InlineKeyboardButton(text='Штампування плюшки', callback_data='bun_stamping'),
-     InlineKeyboardButton(text='Штампування кришки', callback_data='cap stamping')],
+     InlineKeyboardButton(text='Штампування кришки', callback_data='cap_stamping')],
     [InlineKeyboardButton(text='Штампування дна', callback_data='bottom_stamping'),
      InlineKeyboardButton(text='Загибання дна', callback_data='bending_of_bottom')],
     [InlineKeyboardButton(text='Калібровка після\n штамповки плюшки', callback_data='calibration_bun'),
@@ -40,7 +42,58 @@ stamping = InlineKeyboardMarkup(inline_keyboard=[                               
      InlineKeyboardButton(text='Шліфовка болгаркою', callback_data='grinding_cap')],
     [InlineKeyboardButton(text='Заміна матриці', callback_data='matrix_replacement')]
 ])
+
 painting = InlineKeyboardMarkup(inline_keyboard=[                              # вибір робіт
     [InlineKeyboardButton(text='Маляр', callback_data='painter'),
      InlineKeyboardButton(text='Підмайстер', callback_data='journeyman_paint')],
 ])
+##
+preparatory_work = InlineKeyboardMarkup(inline_keyboard=[                                # вибір робіт
+    [InlineKeyboardButton(text='Нарізка штиря', callback_data='pin_cutting'),
+     InlineKeyboardButton(text='Шліфування фаски штиря', callback_data='pin_chamfer_grinding')],
+    [InlineKeyboardButton(text='Миття штирів', callback_data='pin_washing'),
+     InlineKeyboardButton(text='Миття квадратів', callback_data='washing_squares')],
+    [InlineKeyboardButton(text='Нарізка УСБ під ящики', callback_data='cutting_usb_boxes'),
+     InlineKeyboardButton(text='Складання ящиків', callback_data='making_boxes')]
+])
+
+pineapple = InlineKeyboardMarkup(inline_keyboard=[                                # вибір робіт
+    [InlineKeyboardButton(text='Прокат обичайки\n на вальцях', callback_data='roller_rolling'),
+     InlineKeyboardButton(text='Зварювання лазером', callback_data='laser_welding')],
+    [InlineKeyboardButton(text='Точкове зварювання\n гайки з кришкою', callback_data='spot_welding_nut_cover'),
+     InlineKeyboardButton(text='Вальцювання на токарному\n верстаті обичайки', callback_data='rolling_on_lathe')],
+    [InlineKeyboardButton(text='Зварювання кришки з\n гайкою до обичайки', callback_data='welding_cover_to_shell'),
+     InlineKeyboardButton(text='Накрутка смуги\n (первинна)', callback_data='strip_wrapping')],
+    [InlineKeyboardButton(text='Встановлення (вкручування)\n смуги', callback_data='screwing_strip')],
+    [InlineKeyboardButton(text='Затягування смуги та\n приварювання шліфування\n зварних місць',
+                          callback_data='strip_tightening_welding_grinding')]
+])
+
+corn = InlineKeyboardMarkup(inline_keyboard=[                                # вибір робіт
+    [InlineKeyboardButton(text='Прокат обичайки\n на вальцях', callback_data='roller_rolling'),
+     InlineKeyboardButton(text='Зварювання лазером', callback_data='laser_welding')],
+    [InlineKeyboardButton(text='Точкове зварювання\n гайки з кришкою', callback_data='spot_welding_nut_cover'),
+     InlineKeyboardButton(text='Вальцювання на токарному\n верстаті обичайки', callback_data='rolling_on_lathe')],
+    [InlineKeyboardButton(text='Зварювання кришки з\n гайкою до обичайки', callback_data='welding_cover_to_shell'),
+     InlineKeyboardButton(text='Накрутка смуги\n (первинна)', callback_data='strip_wrapping')],
+    [InlineKeyboardButton(text='Встановлення (вкручування)\n смуги', callback_data='screwing_strip')],
+    [InlineKeyboardButton(text='Затягування смуги та\n приварювання шліфування\n зварних місць',
+                          callback_data='strip_tightening_welding_grinding')]
+])
+
+tubes = InlineKeyboardMarkup(inline_keyboard=[                                # вибір робіт
+    [InlineKeyboardButton(text='Нарізка термоусадки\n у розмір', callback_data='cutting_heat_shrink'),
+     InlineKeyboardButton(text='Поклейка штирів', callback_data='gluing_pins')],
+    [InlineKeyboardButton(text='Заклепування квадратів', callback_data='rivet_squares'),
+     InlineKeyboardButton(text='Вкручування штир+\nквадрат+трубка', callback_data='pin_square_tube')],
+    [InlineKeyboardButton(text='Обрізка трубки та\n встановлення заглушки',
+                          callback_data='cutting_tubes_installing_plugs')],
+    [InlineKeyboardButton(text='Первинна герметизація', callback_data='primary_sealing'),
+     InlineKeyboardButton(text='Термоусадження трубки', callback_data='heat_shrink_tube')],
+    [InlineKeyboardButton(text='Підрізування термоусадки у розмір', callback_data='cutting_heat_shrink')],
+    [InlineKeyboardButton(text='Фінальна герметизація.\n Оператор', callback_data='final_sealing_operator'),
+     InlineKeyboardButton(text='Фінальна герметизація.\n Підмайстер', callback_data='final_sealing_journeyman')],
+    [InlineKeyboardButton(text='Фінальна герметизація.\n Різноробочий', callback_data='final_sealing_handyman'),
+     InlineKeyboardButton(text='ВТК упаковка', callback_data='technical_control_package')]
+])
+
--- app/keyboards/workshops_areas_kbs.py
+++ app/keyboards/workshops_areas_kbs.py
@@ -12,13 +12,14 @@ mainWS = InlineKeyboardMarkup(inline_keyboard=[                                 
      InlineKeyboardButton(text='Покраска', callback_data='painting')],
     [InlineKeyboardButton(text='Зборка ананасів', callback_data='pineapple'),
      InlineKeyboardButton(text='Зборка кукурудзи', callback_data='corn')],
-    [InlineKeyboardButton(text='Трубочки', callback_data='straws')]
+    [InlineKeyboardButton(text='Трубочки', callback_data='straws'),
+     InlineKeyboardButton(text='Підготовчі роботи', callback_data='preparatory_work')]
 #     InlineKeyboardButton(text='Термос', callback_data='thermos')]
 ])
 auxiliaryWS = InlineKeyboardMarkup(inline_keyboard=[                              # вибір напрямків
     [InlineKeyboardButton(text='Майстерня', callback_data='repair_shop'),
      InlineKeyboardButton(text='Принтерна', callback_data='printers')],
-    [InlineKeyboardButton(text='Фреза', callback_data='Milling_cutter'),
+    [InlineKeyboardButton(text='Фреза', callback_data='milling_cutter'),
      InlineKeyboardButton(text='Зварювання', callback_data='welding')],
     [InlineKeyboardButton(text='Гільотина', callback_data='guillotine'),
      InlineKeyboardButton(text='Погрузка', callback_data='loading')]
@@ -26,7 +27,7 @@ auxiliaryWS = InlineKeyboardMarkup(inline_keyboard=[                            
 departments = InlineKeyboardMarkup(inline_keyboard=[                              # вибір напрямків
     [InlineKeyboardButton(text='Офіс', callback_data='office'),
      InlineKeyboardButton(text='Склад', callback_data='warehouse')],
-    [InlineKeyboardButton(text='Франція', callback_data='France'),
+    [InlineKeyboardButton(text='Франція', callback_data='france'),
      InlineKeyboardButton(text='Тести', callback_data='tests')],
     [InlineKeyboardButton(text='Електронщики', callback_data='electronics')]
 ])
